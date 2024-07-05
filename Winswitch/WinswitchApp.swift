50453cc337b844725eed8cc77ee5ace4e9274400
rokartur <rokartur@icloud.com>
2024-07-05 14:01:06 +0200

    Initial Commit

---
+++ Winswitch/WinswitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WinswitchApp.swift
+//  Winswitch
+//
+//  Created by artur on 05/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct WinswitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
